3c8a0199f993da2728f9d5549031cc2ad23fbb14
tookitogo <tooki@usa.net>
2022-04-24 17:02:16 +0200

    testing

---
+++ duali2ctesting/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#define SDA2      17
+#define SCL2      18
+uint32_t I2C2_FREQ = 400000;
+
+
+//instantiate second Wire instance
+// TwoWire Wire1 = TwoWire(1);
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  Wire1.begin(SDA2, SCL2, I2C2_FREQ);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
